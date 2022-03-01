a436526f9ad4c3c3d0c372b5141992501d8a2024
gulyasmir <gulyasmir@yandex.ru>
2022-03-01 15:45:20 +0300

    fix

--- src/processes/processes.module.ts
+++ src/processes/processes.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { ProcessesService } from './processes.service';
 import { ProcessesController } from './processes.controller';
 import { MongooseModule } from "@nestjs/mongoose";
-import { Proccess, ProccessSchema } from "./schemas/process.schema";
+import { Process, ProcessSchema } from "./schemas/process.schema";
 
 @Module({
   controllers: [ProcessesController],
   providers: [ProcessesService],
   imports:[
     MongooseModule.forFeature([
-      {name: Proccess.name, schema: ProccessSchema}
+      {name: Process.name, schema: ProcessSchema}
     ])
   ]
 })

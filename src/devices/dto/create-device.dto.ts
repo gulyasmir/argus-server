8cba1aa1bb89243af8ee90a28666688dc0aeca4e
gulyasmir <gulyasmir@yandex.ru>
2022-03-10 16:25:46 +0300

    device  for client

--- src/devices/dto/create-device.dto.ts
+++ src/devices/dto/create-device.dto.ts
@@ -1,6 +1,7 @@
 export  class CreateDeviceDto {
-
   readonly  ip: string;
-
   readonly  time: number;
+  readonly  location: string;
+  readonly  function: string;
+  readonly  title: string;
 }
--- src/devices/dto/update-device-dto.ts
+++ src/devices/dto/update-device-dto.ts
@@ -1,7 +1,7 @@
 export class UpdateDeviceDto {
+  readonly  ip: string;
+  readonly  time: number;
   readonly  location: string;
-
   readonly  function: string;
-
   readonly  title: string;
 }
